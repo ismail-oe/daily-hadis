6bbe7e0a6c31ecda7f7fe23da8f7f9ff8d534f44
I-Oe <i.oe@outlook.de>
2024-05-22 19:46:04 +0200

    Initial Commit

---
+++ hadis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hadis
+//
+//  Created by Ismail Özdemir on 22.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hadis/hadisApp.swift
@@ -0,0 +1,17 @@
+//
+//  hadisApp.swift
+//  hadis
+//
+//  Created by Ismail Özdemir on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct hadisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
